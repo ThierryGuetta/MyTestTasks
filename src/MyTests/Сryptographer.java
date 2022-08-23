7ace508bebcee26cf69482b021a911adade1123d
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-23 14:01:59 +0300

    My Tests

--- src/MyTests/Сryptographer.java
+++ src/MyTests/Сryptographer.java
@@ -4,18 +4,17 @@ import java.util.Arrays;
 
 public class Ñryptographer {
     public static void main(String[] args) {
-        int[] array = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int password = 1337;
-        int[] cryptedArray = crypto(array, password);
+        int[] array = {1, 5, 6, 7, 4, 1, 2, 3, 5, 12};
+        int password = 1997;
+        int[] cryptedArray = crypt(array, password);
         System.out.println(Arrays.toString(cryptedArray));
-        int[] decryptedArray = crypto(cryptedArray, password);
+        int[] decryptedArray = crypt(cryptedArray, password);
         System.out.println(Arrays.toString(decryptedArray));
-
     }
 
-    public static int[] crypto(int[] array, int password) {
+    public static int[] crypt(int[] array, int password) {
         int[] result = new int[array.length];
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < result.length; i++) {
             result[i] = array[i] ^ password;
         }
         return result;
