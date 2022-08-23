2b2698b42cc33e0e84224057eb2e079f72b96a39
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-23 13:27:02 +0300

    My Tests

---
+++ PowerOfTwo.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PowerOfTwo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(getPowerOfTwo(scanner.nextInt()));
+    }
+
+    public static int getPowerOfTwo(int power) {
+        if (power >= 0) {
+            return 1 << power;
+        } else {
+            return 1 >> -power;
+        }
+    }
+}
+
+
+
