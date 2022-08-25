5ab69c88b3e41a6e7f8585b143431feae46898bf
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-25 12:57:05 +0300

    My Tests

--- src/MyTests/Cryptographer.java
+++ src/MyTests/Cryptographer.java
@@ -1,22 +1,38 @@
 package src.MyTests;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Cryptographer {
     public static void main(String[] args) {
-        int[] array = {1, 5, 6, 7, 4, 1, 2, 3, 5, 12};
-        int password = 1997;
-        int[] cryptedArray = crypt(array, password);
-        System.out.println(Arrays.toString(cryptedArray));
-        int[] decryptedArray = crypt(cryptedArray, password);
-        System.out.println(Arrays.toString(decryptedArray));
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter array length: ");
+        int[] array = new int[scanner.nextInt()];
+        System.out.println("Enter data to array (" + array.length + " numbers)");
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        System.out.println("Enter password: ");
+        int password = scanner.nextInt();
+        int[] encrypto = encrypto(array, password);
+        int[] decrypto = decrypto(encrypto, password);
+        System.out.println(Arrays.toString(encrypto));
+        System.out.println(Arrays.toString(decrypto));
     }
 
-    public static int[] crypt(int[] array, int password) {
-        int[] result = new int[array.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = array[i] ^ password;
+    public static int[] encrypto(int[] array, int password) {
+        int[] encrypto = new int[array.length];
+        for (int i = 0; i < encrypto.length; i++) {
+            encrypto[i] = array[i] ^ password;
         }
-        return result;
+        return encrypto;
+    }
+
+    public static int[] decrypto(int[] encryptedArray, int password) {
+        int[] decrypto = new int[encryptedArray.length];
+        for (int i = 0; i < decrypto.length; i++) {
+            decrypto[i] = encryptedArray[i] ^ password;
+        }
+        return decrypto;
     }
 }
