9591b1a9cfd4015da6e32598e87782f61a721641
Andrey <andrey.stolyarov1337@gmail.com>
2022-08-28 16:23:15 +0300

    My Tests

---
+++ src/MyTests/Emoji.java
@@ -0,0 +1,8 @@
+package src.MyTests;
+
+public class Emoji {
+    public static void main(String[] args) {
+        int smile = 0x1F600;
+        System.out.println("Улыбающееся лицо: " + Character.toString(smile));
+    }
+}
