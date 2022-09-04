ea9943f912e4f67205f7243785cba7d72965ce82
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-04 13:45:39 +0300

    My Tests

---
+++ src/MyTests/ClassInfo.java
@@ -0,0 +1,24 @@
+package src.MyTests;
+
+public class ClassInfo {
+    public static void main(String[] args) {
+        String string = "Hello";
+        Class a = String.class;
+        Class b = Integer.class;
+        Class c = "Привет".getClass();
+        Class d = string.getClass();
+        Class e = Boolean.TRUE.getClass();
+        Class f = Object.class;
+        Class g = Integer.class;
+        Class h = ClassInfo.class;
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+    }
+}
