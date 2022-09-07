59019b33e2912377543d8e4a23931ddf574d1db4
Andrey <andrey.stolyarov1337@gmail.com>
2022-09-07 17:29:50 +0300

    MyTests

---
+++ src/MyTests/Test/numbers.java
@@ -0,0 +1,9 @@
+package src.MyTests.Test;
+
+public class numbers {
+    public String object = "Robot";
+
+    public numbers (String model) {
+        System.out.println(object + " " + model);
+    }
+}
---
+++ src/MyTests/Test/test.java
@@ -0,0 +1,11 @@
+package src.MyTests.Test;
+
+public class test {
+    public static void main(String[] args) {
+        numbers robot1 = new numbers("x100a");
+        numbers robot2 = new numbers("x100b");
+        numbers robot3 = new numbers("x100c");
+        numbers robot4 = new numbers("x100d");
+    }
+}
+
