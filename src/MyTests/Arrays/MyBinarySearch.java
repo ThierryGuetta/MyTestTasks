a064cec6342cf38e7d1b9b81ed75235a8d8a1d62
Andrey <andrey.stolyarov1337@gmail.com>
2022-10-06 15:36:04 +0300

    My Array Tasks

---
+++ src/MyTests/Arrays/MyBinarySearch.java
@@ -0,0 +1,23 @@
+package src.MyTests.Arrays;
+
+import java.util.Scanner;
+
+public class MyBinarySearch {
+    public static void main(String[] args) {
+        int[] array = {-29, -12, -5, 0, 3, 5, 7, 21, 23, 35, 54}; //11
+
+        Scanner scanner = new Scanner(System.in);
+        int key = scanner.nextInt();
+        int start = 0;
+        int end = array.length - 1;
+        int pos = -1;
+
+        while (pos == -1 && start <= end) {
+            int mid = (start + end) / 2;
+            if (key == array[mid]) pos = mid;
+            else if (key > array[mid]) start = mid + 1;
+            else if (key < array[mid]) end = mid - 1;
+        }
+        System.out.println(pos);
+    }
+}
