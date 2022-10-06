dfc37d45cd25066779097021da04fd9199597522
Andrey Stolyarov <109091759+ThierryGuetta@users.noreply.github.com>
2022-10-06 22:01:51 +0300

    Delete src/MyTests/SandBox/Test2 directory

--- src/MyTests/SandBox/Test2/Main.java
+++
@@ -1,9 +0,0 @@
-package src.MyTests.SandBox.Test2;
-
-public class Main {
-    public static void main(String[] args) {
-        char c = 'c';
-        int i = c - 2;
-        System.out.println(i);
-    }
-}
