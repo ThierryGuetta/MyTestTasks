40d5c087de04dedfee1b182dfa881b59343c03f5
Andrey <andrey.stolyarov1337@gmail.com>
2022-11-03 15:45:34 +0300

    Multithreading Introduction

--- src/MyTests/Multithreading/Introduction/Test.java
+++ src/MyTests/Multithreading/Introduction/Test.java
@@ -2,21 +2,57 @@ package src.MyTests.Multithreading.Introduction;
 
 public class Test {
     public static void main(String[] args) {
-        Test test = new Test();
-        for (int i = 0; i < 1000; i++) {
-            System.out.println("Hello World!");
-        }
-        Thread thread = new Thread();
-        thread.run();
+
+        myThread thread1 = new myThread();
+        Thread thread2 = new Thread(new Runner());
+
+        Thread thread3 = new Thread(new Runnable() { // by anon Runnable
+            @Override
+            public void run() {
+                for (int i = 0; i < 1000; i++) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                    System.out.println(i + "Anon");
+                }
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+        thread3.start();
+
+        System.out.println("Hello from main thread");
     }
 }
 
-class Thread implements Runnable {
+class myThread extends Thread { //by Thread
 
     @Override
     public void run() {
         for (int i = 0; i < 1000; i++) {
-            System.out.println(i);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i + "Thread");
+        }
+    }
+}
+
+class Runner implements Runnable { //by Runnable
+    @Override
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            System.out.println(i + "Runnable");
         }
     }
 }
